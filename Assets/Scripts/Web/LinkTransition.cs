bfca7572024f2e1504f74168ab246578f9754ff4
Predator2779 <66646630+Predator2779@users.noreply.github.com>
2024-05-13 21:34:13 +0500

    added links to assets

---
+++ Assets/Scripts/Web/LinkTransition.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Web
+{
+    public class LinkTransition : MonoBehaviour
+    {
+        public void GoToLink(string url)
+        {
+            Application.OpenURL(url);
+        }
+    }
+}
---
+++ Assets/Scripts/Web/TextLinkTransition.cs
@@ -0,0 +1,19 @@
+﻿using ModestTree;
+using TMPro;
+using UnityEngine;
+
+namespace Web
+{
+    [RequireComponent(typeof(TMP_Text))]
+    public class TextLinkTransition : LinkTransition
+    {
+        private TMP_Text _tmp;
+
+        private void Awake() => _tmp = GetComponent<TMP_Text>();
+        
+        public void GoToLink()
+        {
+            if (_tmp != null && !_tmp.text.IsEmpty()) Application.OpenURL(_tmp.text);
+        }
+    }
+}
