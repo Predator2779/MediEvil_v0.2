1b34133ce66a8282eed318d5ffb0768520ce37cd
Predator2779 <66646630+Predator2779@users.noreply.github.com>
2024-07-10 14:23:17 +0500

    fixed itemCreator

--- Assets/Scripts/Creators/ItemCreator.cs
+++ Assets/Scripts/Creators/ItemCreator.cs
@@ -1,11 +1,29 @@
-﻿namespace Creators
+﻿using Economy.Items;
+using UnityEngine;
+
+namespace Creators
 {
     public class ItemCreator : AbstractCreator
     {
+        [SerializeField] private Sprite _icon;
+        [SerializeField] private string _name;
+        [SerializeField] private string _description;
+        [SerializeField] private int _count;
+        [SerializeField] private int _price;
+
         protected override void InstantiateUnitComponents() => CreateUnit(_unitPrefabBase);
-        
+
         protected override void Initialize()
         {
+            var item = GetOrCreateItemComponent();
+            item.Icon = _icon;
+            item.Name = _name;
+            item.Description = _description;
+            item.Price = _price;
+            item.Count = _count;
         }
+
+        private Item GetOrCreateItemComponent() => 
+            _unitPrefabBase.TryGetComponent(out Item item) ? item : _unitPrefabBase.AddComponent<Item>();
     }
 }
--- Assets/Scripts/Economy/Items/Item.cs
+++ Assets/Scripts/Economy/Items/Item.cs
@@ -6,10 +6,10 @@ namespace Economy.Items
     {
         [SerializeField] [Min(1)] private int _count = 1;
         
-        [field: SerializeField] public Sprite Icon { get; private set; }
+        [field: SerializeField] public Sprite Icon { get; set; }
         [field: SerializeField] public string Name { get; set; }
-        [field: SerializeField] public string Description { get; private set; }
-        [field: SerializeField] public int Price { get; private set; }
+        [field: SerializeField] public string Description { get; set; }
+        [field: SerializeField] public int Price { get; set; }
         public int Count { get => _count; set => _count = value; }
 
         public abstract void PickUp();
